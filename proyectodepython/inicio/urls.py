36a4e8e3b0595705a2b825b30905ffbeeed869f0
chiara <chiararomanobe@gmail.com>
2026-05-22 01:39:32 -0300

    agregue formulario para crear posteos

--- proyectodepython/inicio/urls.py
+++ proyectodepython/inicio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import inicio
+from .views import inicio, crear_posteo
 
 urlpatterns = [
-    path('', inicio),
+    path('',inicio, name="inicio"),
+    path("crear/", crear_posteo, name="crear_posteo")
 ]
--- proyectodepython/inicio/views.py
+++ proyectodepython/inicio/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Posteo
+from .forms import PosteoForm
 
 def inicio(request):
     posteos = Posteo.objects.all().order_by("-fecha_de_creacion")
     return render(request, 'inicio/inicio.html', {"posteos" : posteos})
+
+def crear_posteo(request):
+    if request.method == "POST":
+        form = PosteoForm(request.POST)
+        
+        if form.is_valid():
+            posteo = form.save(commit=False)
+            posteo.autor = request.user
+            posteo.save()
+            return redirect("inicio")
+
+    else: 
+        form = PosteoForm()
+
+    return render(request, 'inicio/crear_posteo.html', {"form": form})
 
