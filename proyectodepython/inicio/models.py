ef2bcc5bc86ec8a26669194510a9f6b4f4bb5a42
chiara <chiararomanobe@gmail.com>
2026-05-22 00:39:55 -0300

    agrego modelo de posteos

--- proyectodepython/inicio/models.py
+++ proyectodepython/inicio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Posteo(models.Model):
+    titulo=models.CharField(max_length=100)
+    contenido = models.TextField()
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha_de_creacion = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
